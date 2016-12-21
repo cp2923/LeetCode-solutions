ac14a03dae34af5b370df7682138f40d39c168d5
Chang <chang@Changs-MacBook-Pro.local>
2016-12-20 23:39:49 -0500

    2

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -11,4 +11,17 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        
+        summation = tmp = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            tmp1 = tmp2 = 0
+            if l1:
+                tmp1 = l1.val
+                l1 = l1.next
+            if l2:
+                tmp2 = l2.val
+                l2 = l2.next
+            carry, t = divmod(tmp1 + tmp2 + carry, 10)
+            tmp.next = ListNode(t)
+            tmp = tmp.next
+        return summation.next
